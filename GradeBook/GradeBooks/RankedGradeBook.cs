bb582a51c4df409573e63669bca9da0bd030935e
Martin Taylor <martin.taylor@rac.com.au>
2019-07-21 16:40:20 +0800

    Override RankedGradeBooks GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,30 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            switch(averageGrade)
+            {
+                case var grade when grade > 80:
+                    return 'A';
+
+                case var grade when grade > 60:
+                    return 'B';
+
+                case var grade when grade > 40:
+                    return 'C';
+
+                case var grade when grade > 20:
+                    return 'D';
+            }
+
+            return 'F';
+        }
     }
 }
